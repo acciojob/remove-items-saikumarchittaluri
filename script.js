2878f430aa0ef67b3de0efe68a3b9b5d0a28b548
saikumarchittaluri <127985962+saikumarchittaluri@users.noreply.github.com>
2023-08-03 22:16:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+  const colorSelect = document.getElementById("colorSelect");
+  const button = document.querySelector("input[type=button]");
+
+  button.addEventListener("click", () => {
+    const selectedOption = colorSelect.selectedIndex;
+
+    if (selectedOption !== -1) {
+      colorSelect.remove(selectedOption);
+    }
+  });
+});
